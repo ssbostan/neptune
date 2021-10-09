d4fbd990ca550f1ba423d51f499b706c95c31069
Saeid Bostandoust <ssbostan@gmail.com>
2021-10-09 15:17:18 +0330

    adding marshmallow package

--- api/api.py
+++ api/api.py
@@ -1,12 +1,14 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 
 from api.config import Config
 
 db = SQLAlchemy()
 mg = Migrate()
+ma = Marshmallow()
 
 apiv1_bp = Blueprint("apiv1", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
@@ -18,5 +20,6 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     mg.init_app(app, db)
+    ma.init_app(app)
     app.register_blueprint(apiv1_bp) # Register APIv1 blueprint to application.
     return app
